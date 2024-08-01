9c8006181e44a8c60827d5499f3c4d02cb9b4437
shyam <shyamlal@gmail.com>
2024-08-01 18:51:10 +0530

    NewsFeed Update

--- front end/src/Component/NewsFeed/NewsFeed.jsx
+++ front end/src/Component/NewsFeed/NewsFeed.jsx
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './NewsFeed.css';
+//import { UserContext } from '../../UserContext';
 
 
 const NewsFeed = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null); // Added state for error handling
-  //const userId = 2; // This should be dynamically set based on the logged-in user
+  //const user = 2; // This should be dynamically set based on the logged-in user
+  //const user;
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-          //const response = await axios.get(`/api/newsfeed?userId=${userId}`);\
+          //const response = await axios.get(`http://localhost:8080/api/user/${user.id}`);
           const response = await axios.get(`http://localhost:8080/api/post/getallpost`);
           console.log(response.data); // Log the response data
           setPosts(response.data);
@@ -41,11 +43,18 @@ const NewsFeed = () => {
       {posts.length === 0 ? (
         <p>No posts available.</p>
       ) : (
+        
         <ul className="post-list">
           {posts.map(post => (
-            <li key={post.id} className="post-item">
-              <p>{post.content}</p>
+            <li key={post[0]} className="post">
+              <p>Posted by {post.id}</p>
               <small>Posted on: {new Date(post.createDate).toLocaleString()}</small>
+              <p>{post.content}</p>
+              <div className="buttons">
+                <button>Like</button>
+                <button className="comment">Comment</button>
+                <button className="share">Share</button>
+              </div>
             </li>
           ))}
         </ul>
