7f8197edc025b87ed0666f9a15d0e10c60254677
shyam <Shyamlal9802@gmail.com>
2024-08-04 09:19:02 +0530

    Update UserDTO

--- SocialMedia/src/main/java/com/platform/SocialMedia/dto/UserDTO.java
+++ SocialMedia/src/main/java/com/platform/SocialMedia/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package com.platform.SocialMedia.dto;
 
+import com.platform.SocialMedia.Entity.Post;
 import lombok.*;
 
 import java.util.Date;
@@ -17,7 +18,7 @@ public class UserDTO {
     private String profileURL;
     private String bio;
     private Date date;
-//    private List<Post> posts;
-//    private List<UserDTO> followers;
-//    private List<UserDTO> following;
+    private List<Post> posts;
+    private List<UserDTO> followers;
+    private List<UserDTO> following;
 }
