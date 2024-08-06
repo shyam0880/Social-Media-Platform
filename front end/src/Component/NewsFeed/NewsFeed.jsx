51aee67871b82d1d21572889841f810e0fa18a03
shyam <Shyamlal9802@gmail.com>
2024-08-07 00:00:57 +0530

    Update News Feed: Style Posts and User List

--- front end/src/Component/NewsFeed/NewsFeed.jsx
+++ front end/src/Component/NewsFeed/NewsFeed.jsx
@@ -46,6 +46,10 @@ const NewsFeed = () => {
   if (error) {
     return <div>{error}</div>; // Show error message if there's an error
   }
+  const finduser=(userid)=>{
+    const user = users.find(user => user.id === userid);
+    return user.profileURL;
+  }
 
   return (
     <div className='homepage'>
@@ -53,40 +57,62 @@ const NewsFeed = () => {
         <h1>Recent</h1>
       </div>
       <div className="newsfeed-container">
-        <h1>Newsfeed</h1>
-        {posts.length === 0 ? (
-          <p>No posts available.</p>
-        ) : (
-          
-          <ul className="post-list">
-            {posts.map(post => (
-              <li key={post.id} className="post">
-                <p>Posted by {post.fullName}</p>
-                <small>Posted on: {new Date(post.createDate).toLocaleString()}</small>
-                <p>{post.content}</p>
-                <div className="buttons">
-                  <button>Like</button>
-                  <button className="comment">Comment</button>
-                  <button className="share">Share</button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
+        <div className='feedhead'>
+          <h1>News Feed</h1>
+        </div>
+        <div className='feedbody'>
+          {posts.length === 0 ? (
+            <p>No posts available.</p>
+          ) : (
+            <ul className="post-list">
+              {posts.map(post => (
+                <li key={post.id} className="post">
+                  <div className="postuser">
+                    <div className='userimg'>
+                      <img src={finduser(post.authorId)} alt={users.fullName} />
+                    </div>
+                    <div className='userbody'>
+                      <p>{post.fullName}</p>
+                      <small>Posted on: {new Date(post.createDate).toLocaleString()}</small>
+                    </div>
+                  </div>
+                  <div className='content'>
+                    <p>{post.content}</p>
+                  </div>
+                  <div className="buttons">
+                    <button>Like</button>
+                    <button className="comment">Comment</button>
+                    <button className="share">Share</button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
       <div className='users-list'>
-        <h1>User List</h1>
+        <div>
+          <h1>User List</h1>
+        </div>
         <br/>
-        <ul className="post-list">
-            {users.map(users => (
-              <li key={users.id} className="post">
-                <p>{users.firstName+" "+users.lastName}</p>
-                <div className="buttons">
-                  <button>Follow</button>
-                </div>
-              </li>
-            ))}
-          </ul>
+        <div className="users">
+          <ul>
+              {users.map(users => (
+                <li key={users.id} className="eachuser">
+                  <div className='userimg'>
+                    <img src={users.profileURL} alt={users.fullName} />
+                  </div>
+                  <div className='userbody'>
+                    <p>{users.firstName+" "+users.lastName}</p>
+                  </div>
+                  <div className="buttons">
+                    <button>Follow</button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+
+        </div>
       </div>
     </div>
   );
