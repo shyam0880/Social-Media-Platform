c6811434a2f433ab2e4478af4aa7e2727bbc5b69
shyam <Shyamlal9802@gmail.com>
2024-08-19 21:34:07 +0530

    Update Follow and Unfollow DTO

---
+++ SocialMedia/src/main/java/com/platform/SocialMedia/dto/FollowDTO.java
@@ -0,0 +1,14 @@
+package com.platform.SocialMedia.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FollowDTO {
+    private Long userId;
+    private Long followerId;
+}
